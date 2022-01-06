6a3ae62623ae6907c750e18106b475ed8fdf58e7
pingmado <75196272+ping2109@users.noreply.github.com>
2022-01-06 16:36:23 +0700

    Update aosp_beryllium.mk

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,16 +6,19 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
+# GApps
+WITH_GMS := true
+
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_USES_AOSP_RECOVERY := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := cherish_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -28,3 +31,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+# Add CherishOS Maintainer name
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer="ping2109"
